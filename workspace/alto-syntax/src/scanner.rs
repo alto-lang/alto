51c21d2d2009213e1ba8ceb5e9f9e0b714b85823
Zack Pace <pacezipper@gmail.com>
2023-02-24 09:07:07 -0700

    feat: [lexer] implement keyword & punctuation tokens

--- workspace/alto-syntax/src/scanner.rs
+++ workspace/alto-syntax/src/scanner.rs
@@ -5,12 +5,33 @@ use parseme::{
     Error, NoMatchError, Parser, Source,
 };
 
-use crate::token::{Id, Token};
+use crate::{
+    token::{
+        Id, Inst, InstKind, Keyword, KeywordKind, Punct, PunctKind, Symbol, SymbolKind, Token,
+    },
+    Span,
+};
 
 /// An error that occurred during scanning.
-#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub enum ScanError {
+    /// No match could be found.
     NoMatch,
+
+    /// A character was invalid.
+    UnrecognizedCharacter { pos: Span },
+
+    /// A symbol name was expected after an `@` symbol.
+    SymbolNameExpected {
+        /// The position of the `@` before the symbol name.
+        pos: usize,
+    },
+
+    /// Expected a letter or underscore after the `@` symbol.
+    InvalidSymbolName {
+        /// The position of the offending character.
+        pos: usize,
+    },
 }
 
 impl Error for ScanError {
@@ -49,8 +70,31 @@ pub fn skip<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
     Err(ScanError::NoMatch)
 }
 
+/// Scans for a single symbol token.
+pub fn scan_symbol<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
+    let start_pos = input.pos();
+
+    parseme::iter::next_if(input, |c| c == '@').ok_or(ScanError::NoMatch)?;
+
+    if !parseme::xid::is_continue(
+        input
+            .next()
+            .ok_or(ScanError::SymbolNameExpected { pos: start_pos })?,
+    ) {
+        return Err(ScanError::InvalidSymbolName { pos: start_pos + 1 });
+    }
+
+    parseme::iter::advance_while(input, parseme::xid::is_continue);
+
+    Ok(Token::Symbol(Symbol {
+        span: start_pos..input.pos(),
+        kind: SymbolKind::Id,
+        value: &input.src()[start_pos..input.pos()],
+    }))
+}
+
 /// Scans for a single identifier token.
-pub fn scan_id<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
+pub fn scan_id<'a>(input: &mut Source<'a>) -> Result<Id<'a>, ScanError> {
     let start_pos = input.pos();
 
     // supports underscores as the starting character
@@ -58,14 +102,93 @@ pub fn scan_id<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
         .ok_or(ScanError::NoMatch)?;
     parseme::iter::advance_while(input, parseme::xid::is_continue);
 
-    Ok(Token::Id(Id {
+    Ok(Id {
         span: start_pos..input.pos(),
         value: &input.src()[start_pos..input.pos()],
+    })
+}
+
+/// Scans for a single identifier or keyword token.
+pub fn scan_id_or_keyword<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
+    let id = scan_id(input)?;
+
+    Ok(Token::Inst(Inst {
+        span: id.span.clone(),
+        kind: match id.value.to_lowercase().as_str() {
+            "ret" => InstKind::Ret,
+            _ => {
+                return Ok(Token::Keyword(Keyword {
+                    span: id.span.clone(),
+                    kind: match id.value {
+                        "fn" => KeywordKind::Fn,
+                        _ => return Ok(Token::Id(id)),
+                    },
+                }))
+            }
+        },
     }))
+    // Ok(Token::Keyword(Keyword {
+    //     span: id.span.clone(),
+    //     kind: match id.value {
+    //         "fn" => KeywordKind::Fn,
+    //         _ => return Ok(Token::Id(id)),
+    //     },
+    // }))
+}
+
+/// Scans for a single arrow token.
+pub fn scan_arrow<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
+    let start_pos = input.pos();
+
+    if input.peek() != Some('-') && input.peek_nth(1) != Some('>') {
+        return Err(ScanError::NoMatch);
+    }
+
+    input.nth(1);
+
+    Ok(Token::Punct(Punct {
+        span: start_pos..input.pos(),
+        kind: PunctKind::Arrow,
+    }))
+}
+
+/// Scans any single-character punctuation token.
+pub fn scan_single_punct<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
+    let start_pos = input.pos();
+    let kind = match input.peek().ok_or(ScanError::NoMatch)? {
+        '(' => PunctKind::OpenParen,
+        ')' => PunctKind::CloseParen,
+        ':' => PunctKind::Colon,
+        _ => return Err(ScanError::NoMatch),
+    };
+
+    input.next();
+
+    Ok(Token::Punct(Punct {
+        span: start_pos..input.pos(),
+        kind,
+    }))
+}
+
+/// Catches any unrecognized characters.
+pub fn catchall<'a>(input: &mut Source<'a>) -> Result<Token<'a>, ScanError> {
+    let start_pos = input.pos();
+
+    input.next().ok_or(ScanError::NoMatch)?;
+
+    Err(ScanError::UnrecognizedCharacter {
+        pos: start_pos..input.pos(),
+    })
 }
 
 /// Initializes a lexical scanner instance.
 #[inline]
 pub fn new<'a>() -> impl Parser<Source<'a>, Token<'a>, GroupError<ScanError>> {
-    Group::new().add(skip).add(scan_id)
+    Group::new()
+        .add(skip)
+        .add(scan_symbol)
+        .add(scan_id_or_keyword)
+        .add(scan_arrow)
+        .add(scan_single_punct)
+        .add(catchall)
 }
--- workspace/alto-syntax/src/token.rs
+++ workspace/alto-syntax/src/token.rs
@@ -1,14 +1,90 @@
 use crate::Span;
 
 /// An identifier token.
+///
+/// ```alto
+/// _identifier
+/// identifier
+/// i123
+/// ```
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub struct Id<'a> {
     pub span: Span,
     pub value: &'a str,
 }
 
+/// The kind of a [Symbol]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum SymbolKind {
+    /// An identifier was used as the symbol's value.
+    Id,
+
+    /// A string was used as the symbol's value.
+    Str,
+}
+
+/// The name of a symbol.
+///
+/// ```alto
+/// @main
+/// @"str" // TODO
+/// ```
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Symbol<'a> {
+    pub span: Span,
+    pub kind: SymbolKind,
+    pub value: &'a str,
+}
+
+/// The kind of a [Keyword].
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum KeywordKind {
+    Fn,
+}
+
+/// Any keyword token.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Keyword {
+    pub span: Span,
+    pub kind: KeywordKind,
+}
+
+/// The kind of a [Punct].
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum PunctKind {
+    OpenParen,
+    CloseParen,
+    /// `->`
+    Arrow,
+    Colon,
+}
+
+/// Any punctuation token.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Punct {
+    pub span: Span,
+    pub kind: PunctKind,
+}
+
+/// The kind of an [Inst] token.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum InstKind {
+    Ret,
+}
+
+/// Any instruction token.
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct Inst {
+    pub span: Span,
+    pub kind: InstKind,
+}
+
 /// Tokens produced by the Alto lexer.
 #[derive(Clone, Debug, Hash, PartialEq, Eq)]
 pub enum Token<'a> {
     Id(Id<'a>),
+    Symbol(Symbol<'a>),
+    Keyword(Keyword),
+    Punct(Punct),
+    Inst(Inst),
 }
